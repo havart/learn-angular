a53d92a646753e760eccda31f4eddcba3abc188b
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-04-17 17:38:53 +0300

    Revert "add: button+style, click deleteItem, clear input after add"
    This reverts commit dcbbc5e94288754e2fce177f02a789e3457df30f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'learn-angular-change-detaction';
+    title = 'learn-angular-change-detaction';
 
-  items: string[] = ['one', 'two', 'three'];
+    items: string[] = ['one', 'two', 'three'];
 
-  addItem(item) {
-    if (item) {
-      this.items.push(item);
+    addItem(item) {
+        this.items.push(item);
     }
-  }
 }
--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,21 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss'],
+    selector: 'app-child',
+    templateUrl: './child.component.html',
+    styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent implements OnInit {
-  @Input() items: string[];
+    @Input() items: string[];
 
-  constructor() {
-  }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
-  deleteItem(value: string) {
-    const index = this.items.indexOf(value);
-    this.items.splice(index, 1);
-  }
+    ngOnInit() {}
 }
