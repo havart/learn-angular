5bd9949fd48df1eb4e3148be59b2ae0bde6e9db4
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-04-18 14:31:01 +0300

    fix: hover to visible button

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,29 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss'],
+    selector: 'app-child',
+    templateUrl: './child.component.html',
+    styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent implements OnInit {
-  @Input() items: string[];
-  isVisibleButton = false;
+    @Input() items: string[];
+    isVisibleButton = false;
 
-  constructor() {
-  }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
-  deleteItem(index: number) {
-    this.items.splice(index, 1);
-    this.isVisibleButton = false;
-  }
-
-  onVisible(event: Event) {
-    this.isVisibleButton = !this.isVisibleButton;
-    this.isVisibleButton
-        ? (event.currentTarget.children[0].style.visibility = 'visible')
-        : (event.currentTarget.children[0].style.visibility = 'hidden');
-  }
+    deleteItem(index: number) {
+        this.items.splice(index, 1);
+        this.isVisibleButton = false;
+    }
 }
