15089f9b3cfb521e9836efbde94adafe0d225837
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-04-25 15:22:39 +0300

    add: mat-menu to menu-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatButtonModule } from '@angular/material/button';
-import { ToolbarComponent } from './components/smart/toolbar/toolbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatButtonModule} from '@angular/material/button';
+import {ToolbarComponent} from './components/smart/toolbar/toolbar.component';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
-    declarations: [AppComponent, ToolbarComponent],
-    imports: [
-        MatToolbarModule,
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatInputModule,
-        MatCardModule,
-        MatChipsModule,
-        MatButtonModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, ToolbarComponent],
+  imports: [
+    MatToolbarModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
