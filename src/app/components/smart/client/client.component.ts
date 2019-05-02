ea55ee3d53c65fe82ae3394e71f69ee4d142429a
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-05-02 15:30:27 +0300

    fix: get age

--- src/app/components/smart/client/client.component.ts
+++ src/app/components/smart/client/client.component.ts
@@ -1,22 +1,27 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ClientInfoService } from '../../../services/clientInfoService/client-info.service';
-import { IClient } from '../../../interfaces/client.interface';
-import { Observable } from 'rxjs';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ClientInfoService} from '../../../services/clientInfoService/client-info.service';
+import {IClient} from '../../../interfaces/client.interface';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Component({
-    selector: 'app-client',
-    templateUrl: './client.component.html',
-    styleUrls: ['./client.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClientComponent implements OnInit {
-    client$: Observable<IClient>;
-    id: string;
+  client$: Observable<IClient>;
+  id: string;
+  birth: Date;
 
-    constructor(private clientInfoService: ClientInfoService) {}
+  constructor(private clientInfoService: ClientInfoService) {
+  }
 
-    ngOnInit() {
-        this.id = '2';
-        this.client$ = this.clientInfoService.getById(this.id);
-    }
+  ngOnInit() {
+    this.id = '5';
+    this.client$ = this.clientInfoService.getById(this.id).pipe(
+      tap(el => (this.birth = parseInt((new Date() - new Date(el.age)) / 3.154e10 / 1000 / 60 / 60 / 24 / 30, 10))),
+    );
+  }
 }
