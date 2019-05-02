c9fd82460cc8241a60651f6bf757524b46406efc
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-05-02 09:49:12 +0300

    upd: add type to get()

--- src/app/services/stepService/step.service.ts
+++ src/app/services/stepService/step.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API } from '../API';
+import { Observable } from 'rxjs';
+import { StepsInterface } from '../../interfaces/steps.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -8,7 +10,7 @@ import { API } from '../API';
 export class StepService {
     constructor(private httpClient: HttpClient, private config: API) {}
 
-    get() {
+    get(): Observable<StepsInterface>[] {
         return this.httpClient.get(this.config.STEPS_URL);
     }
 }
