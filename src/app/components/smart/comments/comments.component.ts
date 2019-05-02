6114df3ba7fd573939be324923b163593e67a0dc
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-05-02 12:29:07 +0300

    refactor comment subscribe to async

--- src/app/components/smart/comments/comments.component.ts
+++ src/app/components/smart/comments/comments.component.ts
@@ -1,34 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { CommentService } from 'src/app/services/comment.service';
-
-export interface Data {
-    id: string;
-    createdAt: string;
-    name: string;
-    comment: string;
-    viewType: string;
-    isComment: boolean;
-}
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { CommentService } from 'src/app/services/comment/comment.service';
+import { Observable } from 'rxjs';
+import { IComments } from 'src/app/interfaces/comment.interface';
+import { map } from 'rxjs/internal/operators';
 
 @Component({
     selector: 'app-comments',
     templateUrl: './comments.component.html',
     styleUrls: ['./comments.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommentsComponent implements OnInit {
-  url = 'http://5bfff0a00296210013dc7e82.mockapi.io/test/steps';
-  data: Data[];
+    comments$: Observable<IComments[]>;
 
     constructor(private commentServ: CommentService) {}
-
     ngOnInit() {
-      this.commentServ.get(this.url).subscribe(
-        (value: Data[]) => {
-            this.data = value.filter(el => el.isComment);
-        },
-        error => {
-            console.log(error);
-        },
-    );
+        this.comments$ = this.commentServ.get().pipe(map(val => val.filter(el => el.isComment)));
     }
 }
