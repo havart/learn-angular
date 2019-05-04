db82a0067f254ede03b5e82a92eaa780972088d1
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-05-04 10:19:08 +0300

    style prettier

--- src/app/components/smart/client/client.component.ts
+++ src/app/components/smart/client/client.component.ts
@@ -1,32 +1,31 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
-import {ClientInfoService} from '../../../services/clientInfoService/client-info.service';
-import {IClient} from '../../../interfaces/client.interface';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {getAge} from '../../../helpers/user-age';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ClientInfoService } from '../../../services/clientInfoService/client-info.service';
+import { IClient } from '../../../interfaces/client.interface';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { getAge } from '../../../helpers/user-age';
 
 @Component({
-  selector: 'app-client',
-  templateUrl: './client.component.html',
-  styleUrls: ['./client.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-client',
+    templateUrl: './client.component.html',
+    styleUrls: ['./client.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClientComponent implements OnInit {
-  client$: Observable<IClient>;
-  clientId: string;
+    client$: Observable<IClient>;
+    clientId: string;
 
-  constructor(private clientInfoService: ClientInfoService) {
-  }
+    constructor(private clientInfoService: ClientInfoService) {}
 
-  ngOnInit() {
-    this.clientId = Math.floor(Math.random() * 10 + 1);
-    this.client$ = this.clientInfoService.getById(this.clientId).pipe(
-      map(el => {
-        return {
-          ...el,
-          age: getAge(el.age),
-        };
-      }),
-    );
-  }
+    ngOnInit() {
+        this.clientId = Math.floor(Math.random() * 10 + 1);
+        this.client$ = this.clientInfoService.getById(this.clientId).pipe(
+            map(el => {
+                return {
+                    ...el,
+                    age: getAge(el.age),
+                };
+            }),
+        );
+    }
 }
