8627704382b00dcb8378e96546ac5e631e9a6218
Snezhana Novitskaya <s.novitskaya@andersenlab.com>
2019-05-04 10:04:50 +0300

    fix: get client -> map

--- src/app/components/smart/client/client.component.ts
+++ src/app/components/smart/client/client.component.ts
@@ -1,28 +1,32 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ClientInfoService } from '../../../services/clientInfoService/client-info.service';
-import { IClient } from '../../../interfaces/client.interface';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { getAge } from '../../../helpers/user-age';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ClientInfoService} from '../../../services/clientInfoService/client-info.service';
+import {IClient} from '../../../interfaces/client.interface';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {getAge} from '../../../helpers/user-age';
 
 @Component({
-    selector: 'app-client',
-    templateUrl: './client.component.html',
-    styleUrls: ['./client.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClientComponent implements OnInit {
-    client$: Observable<IClient>;
-    id: string;
+  client$: Observable<IClient>;
+  clientId: string;
 
-    constructor(private clientInfoService: ClientInfoService) {}
+  constructor(private clientInfoService: ClientInfoService) {
+  }
 
-    ngOnInit() {
-        this.id = Math.floor(Math.random() * 10 + 1);
-        this.client$ = this.clientInfoService.getById(this.id).pipe(
-            tap(el => {
-                return (el.age = getAge(el.age));
-            }),
-        );
-    }
+  ngOnInit() {
+    this.clientId = Math.floor(Math.random() * 10 + 1);
+    this.client$ = this.clientInfoService.getById(this.clientId).pipe(
+      map(el => {
+        return {
+          ...el,
+          age: getAge(el.age),
+        };
+      }),
+    );
+  }
 }
