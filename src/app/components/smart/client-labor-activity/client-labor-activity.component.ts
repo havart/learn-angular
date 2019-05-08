c47c85f94f77eef13bfb61fc428a002751745ae6
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-05-08 12:29:56 +0300

    add OnPush strategy

--- src/app/components/smart/client-labor-activity/client-labor-activity.component.ts
+++ src/app/components/smart/client-labor-activity/client-labor-activity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ClientInfoService } from 'src/app/services/clientInfoService/client-info.service';
 import { Observable } from 'rxjs';
 import { ILabor } from 'src/app/interfaces/labor.interface';
@@ -7,6 +7,7 @@ import { ILabor } from 'src/app/interfaces/labor.interface';
     selector: 'app-client-labor-activity',
     templateUrl: './client-labor-activity.component.html',
     styleUrls: ['./client-labor-activity.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClientLaborActivityComponent implements OnInit {
     client$: Observable<ILabor>;
