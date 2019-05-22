b65ba8a8661ea06edf9ff5a8ce39fb98cf804191
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-05-22 11:48:06 +0300

    add: get labor from store

--- src/app/components/smart/labor-activity-form/labor-activity-form.component.ts
+++ src/app/components/smart/labor-activity-form/labor-activity-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ILabor } from 'src/app/interfaces/labor.interface';
-// import { ClientInfoService } from 'src/app/services/clientInfoService/client-info.service';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { ClientInfoService } from 'src/app/services/clientInfoService/client-info.service';
+import { Observable } from 'rxjs';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { Store, select } from '@ngrx/store';
-import {  UpsertLabor } from 'src/app/store/actions/client-labor.action';
+import { Store } from '@ngrx/store';
+import { UpsertLabor } from 'src/app/store/actions/client-labor.action';
 import { LABOR } from 'src/app/store/selectors/client-labor.selector';
+import { tap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-labor-activity-form',
@@ -17,34 +18,28 @@ export class LaborActivityFormComponent implements OnInit {
     form: FormGroup = new FormGroup({});
     employments = ['Кастелян', 'Министр', 'Диктатор', 'Глава государства', 'Креативный директор', 'Директор'];
     client$: Observable<ILabor>;
-    laborState$: Observable<ILabor>;
-    // temp = this.store.pipe(select(getSelectLabor));
-    // client$;
 
     constructor(
-        // private clientInfoService: ClientInfoService,
+        private clientInfoService: ClientInfoService,
         private store: Store<ILabor>,
         private formBuilder: FormBuilder,
     ) {
-        // this.store.dispatch(new LoadLabor());
-        // this.client$ = this.store.select(getLabor1);
+        this.client$ = this.store.select(LABOR);
     }
     ngOnInit() {
-        //   this.client$ = this.clientInfoService.getLaborById$();
-        this.client$ = this.store.pipe(select('id'));
-        // this.store.dispatch(new UpsertLabor());
-        // this.temp = this.store.pipe(select('type'));
-        console.log(this.client$);
-        // console.log(this.client$ );
-        // this.store.pipe(select(GetLabor));
-
-        // this.client$ = this.clientInfoService.labor$;
-        // this.clientInfoService.getLaborById$();
+        this.clientInfoService
+            .getLaborById$()
+            .pipe(
+                tap((el: ILabor) => {
+                    this.store.dispatch(new UpsertLabor(el)), this.form.patchValue(el);
+                }),
+            )
+            .subscribe();
         this.initForm();
         this.fillForm();
     }
 
-    private initForm() {
+    private initForm(): void {
         this.form = this.formBuilder.group({
             employment: [null],
             occupation: [null],
@@ -61,24 +56,21 @@ export class LaborActivityFormComponent implements OnInit {
         });
     }
 
-    private fillForm() {
-        // this.client$.forEach(el => this.form.patchValue(el));
-        setTimeout(() => {
-            this.client$.forEach(el => this.form.patchValue(el));
-        }, 2000);
+    private fillForm(): void {
+        this.client$.forEach(el => this.form.patchValue(el));
     }
 
-    save() {
-      //  this.client$ = this.store.pipe(select(getSelectLabor));
+    save(): void {
+        //  this.client$ = this.store.pipe(select(getSelectLabor));
         // this.clientInfoService.updateLabor(this.form.value, this.form.value.id);
         //  this.clientInfoService.getLaborById$();
     }
-    cancel() {
+    cancel(): void {
         this.fillForm();
     }
 
-    add() {
-      //  this.client$ = this.store.pipe(select(getSelectLabor));
+    add(): void {
+        //  this.client$ = this.store.pipe(select(getSelectLabor));
         // this.clientInfoService.addLabor(this.form.value);
         //  this.form.reset();
     }
