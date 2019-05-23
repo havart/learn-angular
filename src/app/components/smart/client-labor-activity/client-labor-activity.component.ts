06d65b3517ac4fec5af27197b83f5ba7b0cb8eec
DESKTOP-82V186T\natal <natalia.nestertsova@gmail.com>
2019-05-23 15:09:31 +0300

    add store in client labor activity

--- src/app/components/smart/client-labor-activity/client-labor-activity.component.ts
+++ src/app/components/smart/client-labor-activity/client-labor-activity.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ClientInfoService } from 'src/app/services/clientInfoService/client-info.service';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { ILabor } from 'src/app/interfaces/labor.interface';
+import { tap } from 'rxjs/operators';
+import { Store, select } from '@ngrx/store';
+import { IAppState } from 'src/app/store/state/app.state';
+import { getLabor } from 'src/app/store/selectors/client-labor.selector';
+import { UpsertLabor, SelectedLaborSet } from 'src/app/store/actions/client-labor.action';
 
 @Component({
     selector: 'app-client-labor-activity',
@@ -12,9 +17,20 @@ import { ILabor } from 'src/app/interfaces/labor.interface';
 export class ClientLaborActivityComponent implements OnInit {
     client$: Observable<ILabor>;
 
-    constructor(private clientInfoService: ClientInfoService) {}
+    constructor(private clientInfoService: ClientInfoService, private store$: Store<IAppState>) {
+        this.client$ = this.store$.pipe(select(getLabor));
+    }
 
     ngOnInit() {
-        this.client$ = this.clientInfoService.getLaborById$();
+        this.clientInfoService
+            .getLaborById$(1)
+            .pipe(
+                tap((el: ILabor) => {
+                    this.store$.dispatch(new UpsertLabor(el));
+                    this.store$.dispatch(new SelectedLaborSet(el.id));
+                }),
+            )
+            .subscribe();
+        // this.client$ = this.clientInfoService.getLaborById$(1);
     }
 }
