691a78ada479ffb9a5586589825492f5dd30f510
User <sHarveyWilliams@gmail.com>
2019-07-03 15:45:09 +0300

    ok

--- src/app/operator-base.guard.ts
+++ src/app/operator-base.guard.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthorizationService } from './services/authorization.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class OperatorBaseGuard implements CanActivate {
+    constructor(private authorizationService: AuthorizationService) {}
+
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-        return true;
+        return this.authorizationService.checkAuth();
     }
 }
