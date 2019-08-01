a69e2114e7c618ba190d4204fc7068364c14118c
Ярослав Максимов <sharveywilliams@gmail.com>
2019-08-01 15:53:22 +0300

    Добавлена стратегия OnPush

--- src/app/components/dumb/operator-main-tabs/operator-main-tabs.component.ts
+++ src/app/components/dumb/operator-main-tabs/operator-main-tabs.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-operator-main-tabs',
   templateUrl: './operator-main-tabs.component.html',
-  styleUrls: ['./operator-main-tabs.component.scss']
+  styleUrls: ['./operator-main-tabs.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OperatorMainTabsComponent implements OnInit {
 
