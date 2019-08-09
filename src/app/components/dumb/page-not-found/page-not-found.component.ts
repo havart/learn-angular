4efb2e45944f094bb37a18f76c182d223f418a5e
Vitaly <v.klykin@gmail.com>
2019-08-09 13:19:00 +0300

    fix bugs routings

---
+++ src/app/components/dumb/page-not-found/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-page-not-found',
+    template: '<h3> Страница не найдена</h3>',
+    styles: ['h3{text-align:center}'],
+})
+export class PageNotFoundComponent {}
