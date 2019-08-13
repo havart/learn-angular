3fac4fc210fec2f09b5c8a4344b761fc6cd9c1fa
Ярослав Максимов <sharveywilliams@gmail.com>
2019-08-13 14:39:28 +0300

    Форма теперь это отдельный компонент

--- src/app/components/feature/contact-tab/contact-tab.component.ts
+++ src/app/components/feature/contact-tab/contact-tab.component.ts
@@ -1,6 +1,4 @@
-import { ChangeDetectionStrategy, Component, ContentChild, ElementRef, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ContactTabEnum } from './contact-tab.enum';
+import { Component, OnInit } from '@angular/core';
 import { ContactTabService } from '../../../services/contact-tab.service';
 import { Observable } from 'rxjs';
 import { ContactTabInterface } from '../../../interfaces/contact-tab.interface';
--- src/app/components/smart/contact/contact.component.ts
+++ src/app/components/smart/contact/contact.component.ts
@@ -1,15 +1,29 @@
 import { Component, ContentChild, ElementRef, Input, OnInit } from '@angular/core';
 import { ContactTabInterface } from '../../../interfaces/contact-tab.interface';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ContactEnum } from './contact.enum';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+    selector: 'app-contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent implements OnInit {
-  @Input() itemContact: ContactTabInterface;
+export class ContactComponent {
+    contactForm: FormGroup;
+    @Input() itemContact: ContactTabInterface;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.initContactForm();
+    }
 
+    initContactForm(): void {
+        this.contactForm = new FormGroup({
+            [ContactEnum.NUMBERPHONE]: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+            [ContactEnum.FULLNAME]: new FormControl('', [Validators.required, Validators.maxLength(99)]),
+            [ContactEnum.PHONETYPE]: new FormControl('', Validators.required),
+            [ContactEnum.DEPARTAMENT]: new FormControl('', Validators.required),
+            [ContactEnum.COMMENT]: new FormControl('', [Validators.required, Validators.maxLength(1000)]),
+        });
+    }
+    submit() {}
 }
