30948344fc86521c7bebf9a03410770e20507383
Ярослав Максимов <sharveywilliams@gmail.com>
2019-08-20 10:55:44 +0300

    Решил конфликты

--- src/app/components/dumb/step-with-phone-number/step-with-phone-number.component.ts
+++ src/app/components/dumb/step-with-phone-number/step-with-phone-number.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { StepInterface } from '../../../interfaces/step.interface';
 
 @Component({
     selector: 'app-step-with-phone-number',
     templateUrl: './step-with-phone-number.component.html',
     styleUrls: ['./step-with-phone-number.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StepWithPhoneNumberComponent {
     @Input() step: StepInterface;
 
-    private isShowDescriptionMenu: boolean;
+    isShowDescriptionMenu: boolean;
 
-    private showDescriptionMenuToggle(): void {
+    showDescriptionMenuToggle(): void {
         this.isShowDescriptionMenu = !this.isShowDescriptionMenu;
     }
 }
