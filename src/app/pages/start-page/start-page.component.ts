d0aada095138d5d7cd71069db08251b0a92d9f9e
Sergio Orlov <sergioorlove@gmail.com>
2019-10-23 15:58:55 +0300

    Fix some type of methods.

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { GetTaskService } from '../../services/get-task.service';
 import { getRandomIdHelper } from '../../helpers/get-random-id.helper';
+import { ClientInterface } from '../../interfaces/client.interface';
 
 @Component({
     selector: 'app-start-page',
@@ -14,8 +15,8 @@ export class StartPageComponent {
     getTask() {
         const id = getRandomIdHelper(1, 20);
         this.getTaskService.getClient$(id).subscribe(
-            respond => {
-                console.log(respond);
+          (client: ClientInterface) => {
+                console.log(client);
             },
             (error: HttpErrorResponse) => {
                 console.log(`No data found with id=${id}.`, error);
