cea71fac32e0560d56a04ee33a9a6e595e6b70c5
Sergio Orlov <sergioorlove@gmail.com>
2019-10-24 16:09:20 +0300

    Add routing and some fix.

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserAuthService } from '../services/user-auth.service';
+import { RouterEnum } from '../app-routing/router-enum/router.enum';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+    constructor(private router: Router, private userAuthService: UserAuthService) {}
+
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot,
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        if (!this.userAuthService.checkUser()) {
+            this.router.navigate([`${RouterEnum.LOGIN_PAGE}`]);
+            return false;
+        }
+        return true;
+    }
+}
