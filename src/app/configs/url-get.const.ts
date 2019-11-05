89a5c5333125837d121849d438e323b9fe6d2d11
Sergio Orlov <sergioorlove@gmail.com>
2019-11-05 15:12:32 +0300

    [heroku] add some fix for heroku service

--- src/app/configs/url-get.const.ts
+++ src/app/configs/url-get.const.ts
@@ -1,3 +1,3 @@
-export const basePath = 'https://5bfff0a00296210013dc7e82.mockapi.io/test';
-export const urlGetUser = `${basePath}/user-info/`;
-export const urlGetStep = `${basePath}/steps`;
+export const basePath: string = 'https://5bfff0a00296210013dc7e82.mockapi.io/test';
+export const urlGetUser: string = `${basePath}/user-info/`;
+export const urlGetStep: string = `${basePath}/steps`;
