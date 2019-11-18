0049de7e15ae0d9c2a9621bd8be4753bd62a9962
s.orlov <sergioorlove@gmail.com>
2019-11-18 19:05:28 +0300

    server response constants added and error snackBar

--- src/app/services/error-snack-bar.service.ts
+++ src/app/services/error-snack-bar.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+    BAD_GATEWAY,
+    BAD_REQUEST,
+    FORBIDDEN,
+    GATEWAY_TIMEOUT,
+    INTERNAL_SERVER_ERROR,
+    NOT_FOUND,
+    REQUEST_URL_TOO_LONG,
+    SERVICE_UNAVAILABLE,
+    UNAUTHORIZED,
+} from '../constants/server-response.constants';
 
 @Injectable({
     providedIn: 'root',
@@ -11,15 +22,40 @@ export class ErrorSnackBarService {
         let mes: string;
 
         switch (message) {
-            case 404:
-                mes = 'По данному URL ничего не найдено — данные не существуют!';
+            case BAD_REQUEST:
+                mes = 'Запрос не может быть исполнен ввиду синтаксической ошибки.';
+                break;
+            case UNAUTHORIZED:
+                mes = 'Для получения запрашиваемого ресурса нужна аутентификация.';
+                break;
+            case FORBIDDEN:
+                mes = 'Нет прав доступа к содержимому запроса.';
+                break;
+            case NOT_FOUND:
+                mes = 'По данному URL ничего не найдено — данные не существуют.';
+                break;
+            case REQUEST_URL_TOO_LONG:
+                mes = 'Сервер не может обработать запрос из-за слишком длинного указанного URI.';
+                break;
+            case INTERNAL_SERVER_ERROR:
+                mes = 'Ошибка сервера.';
+                break;
+            case BAD_GATEWAY:
+                mes = 'Ошибка сервера.';
+                break;
+            case SERVICE_UNAVAILABLE:
+                mes = 'Сервер временно не может обрабатывать запросы по техническим причинам.';
+                break;
+            case GATEWAY_TIMEOUT:
+                mes = 'Недействительный (недопустимый) ответ сервера.';
                 break;
             default:
                 mes = 'Undefined!';
                 break;
         }
+
         this.snackBar.open(mes, 'x', {
-            duration: 5000,
+            duration: 6000,
             verticalPosition: 'top',
         });
     }
--- src/app/services/get-comment.service.ts
+++ src/app/services/get-comment.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { EMPTY, Observable } from 'rxjs';
 import { StepInterface } from '../interfaces/step.interface';
 import { urlGetStep } from '../configs/url-get.const';
-import { map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { ErrorSnackBarService } from './error-snack-bar.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GetCommentService {
-    constructor(private readonly http: HttpClient) {}
+    constructor(private readonly http: HttpClient, private readonly errorSnackBarService: ErrorSnackBarService) {}
 
     public getComment$(): Observable<StepInterface[]> {
-        return this.http
-            .get<StepInterface[]>(urlGetStep)
-            .pipe(map((steps: StepInterface[]) => steps.filter(({ isComment }: StepInterface) => isComment)));
+
+        return this.http.get<StepInterface[]>(urlGetStep).pipe(
+            catchError((error: HttpErrorResponse) => {
+                this.errorSnackBarService.openSnackBarError(error.status);
+
+                return EMPTY;
+            }),
+            map((steps: StepInterface[]) => steps.filter(({ isComment }: StepInterface) => isComment)),
+        );
     }
 }
