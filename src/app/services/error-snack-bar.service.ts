afee5885a8f0e56e71689a7281663c2c4652fef9
s.orlov <sergioorlove@gmail.com>
2019-11-19 17:20:34 +0300

    some fix message

--- src/app/services/error-snack-bar.service.ts
+++ src/app/services/error-snack-bar.service.ts
@@ -1,16 +1,6 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import {
-    BAD_GATEWAY,
-    BAD_REQUEST,
-    FORBIDDEN,
-    GATEWAY_TIMEOUT,
-    INTERNAL_SERVER_ERROR,
-    NOT_FOUND,
-    REQUEST_URL_TOO_LONG,
-    SERVICE_UNAVAILABLE,
-    UNAUTHORIZED,
-} from '../constants/server-response.constants';
+import { serverResponseMessages } from '../configs/server-response-messages.const';
 
 @Injectable({
     providedIn: 'root',
@@ -20,42 +10,10 @@ export class ErrorSnackBarService {
 
     openSnackBarError(message: number): void {
         let mes: string;
-
-        switch (message) {
-            case BAD_REQUEST:
-                mes = 'Запрос не может быть исполнен ввиду синтаксической ошибки.';
-                break;
-            case UNAUTHORIZED:
-                mes = 'Для получения запрашиваемого ресурса нужна аутентификация.';
-                break;
-            case FORBIDDEN:
-                mes = 'Нет прав доступа к содержимому запроса.';
-                break;
-            case NOT_FOUND:
-                mes = 'По данному URL ничего не найдено — данные не существуют.';
-                break;
-            case REQUEST_URL_TOO_LONG:
-                mes = 'Сервер не может обработать запрос из-за слишком длинного указанного URI.';
-                break;
-            case INTERNAL_SERVER_ERROR:
-                mes = 'Ошибка сервера.';
-                break;
-            case BAD_GATEWAY:
-                mes = 'Ошибка шлюза.';
-                break;
-            case SERVICE_UNAVAILABLE:
-                mes = 'Сервер временно не может обрабатывать запросы по техническим причинам.';
-                break;
-            case GATEWAY_TIMEOUT:
-                mes = 'Недействительный (недопустимый) ответ сервера.';
-                break;
-            default:
-                mes = 'Undefined!';
-                break;
-        }
+        mes = serverResponseMessages[message] || serverResponseMessages.OTHER_MESSAGE;
 
         this.snackBar.open(mes, 'x', {
-            duration: 6000,
+            duration: 5000,
             verticalPosition: 'top',
         });
     }
