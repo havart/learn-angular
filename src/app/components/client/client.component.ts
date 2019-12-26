dd322de79b0857333b20591e46822953b73e8c25
s.orlov <sergioorlove@gmail.com>
2019-12-26 19:34:50 +0300

    fix some .scss on project.

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
+import { ClientActivityInterface } from '../../interfaces/client-activity.interface';
 import { ClientInterface } from '../../interfaces/client.interface';
 import { ActivatedRoute } from '@angular/router';
+import { ActivityService } from '../../services/activity.service';
 import { ClientService } from '../../services/client.service';
 import { Observable } from 'rxjs';
 
@@ -14,10 +16,18 @@ import { Observable } from 'rxjs';
 export class ClientComponent implements OnInit {
     public time = Date.now();
     public client$: Observable<ClientInterface>;
+    public clientDetail$: Observable<ClientActivityInterface>;
 
-    constructor(private readonly route: ActivatedRoute, private readonly clientService: ClientService) {}
+    constructor(
+        private readonly route: ActivatedRoute,
+        private readonly clientService: ClientService,
+        private readonly activityService: ActivityService,
+    ) {}
 
     ngOnInit(): void {
         this.client$ = this.route.paramMap.pipe(switchMap(params => this.clientService.client$(params.get('id'))));
+        this.clientDetail$ = this.route.paramMap.pipe(
+            switchMap(params => this.activityService.fetchClientActivity$(params.get('id'))),
+        );
     }
 }
