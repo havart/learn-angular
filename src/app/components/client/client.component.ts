f4ba951eb7ad323f87c4885ae4a43c81759076fc
OlgaMart112 <opam112@ukr.net>
2019-12-27 17:59:31 +0200

    fixed css style

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -4,6 +4,8 @@ import { ClientInterface } from '../../interfaces/client.interface';
 import { ActivatedRoute } from '@angular/router';
 import { ClientService } from '../../services/client.service';
 import { Observable } from 'rxjs';
+import { ActivityService } from 'src/app/services/activity.service';
+import { ClientActivityInterface } from 'src/app/interfaces/client-activity.interface';
 
 @Component({
     selector: 'app-client',
@@ -14,10 +16,18 @@ import { Observable } from 'rxjs';
 export class ClientComponent implements OnInit {
     public time = Date.now();
     public client$: Observable<ClientInterface>;
+    public clientActivity$: Observable<ClientActivityInterface>;
 
-    constructor(private readonly route: ActivatedRoute, private readonly clientService: ClientService) {}
+    constructor(
+        private readonly route: ActivatedRoute,
+        private readonly clientService: ClientService,
+        private readonly activityService: ActivityService,
+    ) {}
 
     ngOnInit(): void {
         this.client$ = this.route.paramMap.pipe(switchMap(params => this.clientService.client$(params.get('id'))));
+        this.clientActivity$ = this.route.paramMap.pipe(
+            switchMap(params => this.activityService.fetchClientActivity$(params.get('id'))),
+        );
     }
 }
